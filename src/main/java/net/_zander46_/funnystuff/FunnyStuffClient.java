5383d8e6abcd382b98750506165e287ca1b657d4
zander3285 <zander3285@gmail.com>
2025-01-18 23:40:28 +0500

    initial commit

---
+++ src/main/java/net/_zander46_/funnystuff/FunnyStuffClient.java
@@ -0,0 +1,10 @@
+package net._zander46_.funnystuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FunnyStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/_zander46_/funnystuff/FunnyStuffDataGenerator.java
@@ -0,0 +1,11 @@
+package net._zander46_.funnystuff;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FunnyStuffDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
